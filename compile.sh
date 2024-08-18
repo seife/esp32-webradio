e6e8154ac9e072bb17fcaf80c6964104c0bb5dbd
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2024-08-18 14:02:27 +0200

    compile.sh: enable more compile warnings

--- compile.sh
+++ compile.sh
@@ -13,9 +13,9 @@ if [ "$IAM" = upload ]; then
 	shift
 else
 	# this avoids having to change the config of ESP32-audioI2S
-	PARAM=(--build-property "build.extra_flags.esp32=-DARDUINO_USB_CDC_ON_BOOT=0 -DSBR_DEC=1")
+	PARAM=(--build-property "build.extra_flags.esp32=-DARDUINO_USB_CDC_ON_BOOT=0 -DSBR_DEC=1 -W -Wextra -Wall")
 	# use local deps subdirectory
-	PARAM+=(--libraries deps)
+	PARAM+=(--libraries deps --warnings all)
 fi
 # for some reason, this is not always enough to make PSRAM work. Full params are extracted from IDE GUI build
 #BOARD_PARAM=esp32:esp32:esp32:PSRAM=enabled,PartitionScheme=min_spiffs
